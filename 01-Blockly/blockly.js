c7d9188b454adb45dd763647f40d4e4e58a85d4a
Your Name Here <ebrimaj604@gmail.com>
2021-02-19 11:53:14 +0100

    exercise 1

--- 01-Blockly/blockly.js
+++ 01-Blockly/blockly.js
@@ -1,1 +1,13 @@
 //Insert level 9 code below
+while (notDone()) {
+    if (isPathForward()) {
+      moveForward();
+    } else {
+      if (isPathLeft()) {
+        turnLeft();
+        if (isPathRight()) {
+          turnRight();
+        }
+      }
+    }
+  }
